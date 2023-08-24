f47946ae66913a5c7d4fe57557f37c8cdc7cad45
Di Depan Layar <albinmaulana@yahoo.com>
2023-08-24 09:38:11 +0000

    Feature: Customize Users Table

--- BookManagement.sh
+++ BookManagement.sh
@@ -2,3 +2,5 @@ composer require laravel/ui;
 php artisan ui bootstrap --auth;
 npm install && npm run dev;
 php artisan migrate;
+php artisan make:migration customize_users_table;
+php arisan migrate;
