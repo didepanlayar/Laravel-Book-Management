a404286a813311add1126936a75cbb42ef4f29d9
Di Depan Layar <albinmaulana@yahoo.com>
2023-09-13 23:08:30 +0700

    Refactor: Remove Registration Feature

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::match(['GET', 'POST'], '/register', function() {
+    return redirect('/login');
+})->name('register');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
